ddb67724ea285f57daf9ef143988814f05492303
nueij0429 <147960508+nueij0429@users.noreply.github.com>
2025-05-12 21:26:33 +0900

    [level 1] Title: 소수 찾기, Time: 394.94 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,10 +1,8 @@
 def solution(n):
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
+    num = set(range(2, n+1))
 
-    for i in range(2, int(n**0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-
-    return sum(sieve)
+    for i in range(2, n+1):
+        if i in num:
+            num -= set(range(2 * i, n + 1, i))
+            
+    return len(num)
