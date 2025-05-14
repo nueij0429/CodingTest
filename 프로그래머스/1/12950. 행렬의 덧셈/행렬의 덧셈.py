b50abd62ab22c10dd5cf8d959c2d0e976c996cb0
nueij0429 <147960508+nueij0429@users.noreply.github.com>
2025-05-14 09:33:59 +0900

    [level 1] Title: 행렬의 덧셈, Time: 31.06 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,10 +1,8 @@
-def solution(arr1, arr2):
-    answer = []
+import numpy as np
+
+def solution(A, B):
+    A_np = np.array(A)
     
-    for i in range(len(arr1)): #열 구하기
-        row = []
-        for j in range(len(arr1[0])): #행 구하기
-            row.append(arr1[i][j] + arr2[i][j])
-        answer.append(row)
+    B_np = np.array(B)
     
-    return answer
+    return (A_np + B_np).tolist()
