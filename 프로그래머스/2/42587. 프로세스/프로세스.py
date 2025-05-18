00cd011a9ff96feef2211b77fc82de3d76cab073
nueij0429 <147960508+nueij0429@users.noreply.github.com>
2025-05-18 13:54:11 +0900

    [level 2] Title: 프로세스, Time: 1.36 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,17 +2,19 @@ from collections import deque
 
 def solution(priorities, location):
     dq = deque()
-    count = 0
-    for i in range(len(priorities)):
-        dq.append((priorities[i],i))
+    count = 0 #실행된 프로세스의 순서
     
+    for i in range(len(priorities)):
+        dq.append((priorities[i], i))
+        
     while dq:
         max_pri = max(dq)[0]
         process = dq.popleft()
-        if process[0]==max_pri:
-            count+=1 
-            if process[1] ==location:
+        if process[0] == max_pri:
+            count += 1
+            if process[1] == location:
                 return count
         else:
             dq.append(process)
+        
     return count
