36ae25977f907516ed7d2cb54928950883caa1d0
nueij0429 <147960508+nueij0429@users.noreply.github.com>
2025-05-24 14:46:56 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 8일차 - subtree, Time: 73 ms, Memory: 58,112 KB -BaekjoonHub

--- SWEA/D2/5174. ［파이썬 S／W 문제해결 기본］ 8일차 － subtree/［파이썬 S／W 문제해결 기본］ 8일차 － subtree.py
+++ SWEA/D2/5174. ［파이썬 S／W 문제해결 기본］ 8일차 － subtree/［파이썬 S／W 문제해결 기본］ 8일차 － subtree.py
@@ -1,7 +1,9 @@
 T = int(input())
+
 def dfs(N):
     global count
     count += 1
+    
     if N in graph:
         for i in graph[N]:
             dfs(i)
@@ -11,7 +13,6 @@ def dfs(N):
 for test_case in range(1, T + 1):
     E, N = map(int, input().split())
     arr = list(map(int, input().split()))
-    
     graph = dict()
     
     for i in range(0, len(arr), 2):
@@ -22,4 +23,4 @@ for test_case in range(1, T + 1):
             
     count = 0
     dfs(N)
-    print(f"#{test_case} {count}")
+    print(f'#{test_case} {count}')
